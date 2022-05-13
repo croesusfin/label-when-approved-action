faa8ecbcd18e706e53abadf8aacaad59939cad96
Maxime Gagnon <94866705+maxime-gagnon-croesus@users.noreply.github.com>
2022-05-13 15:49:41 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ fi
 
 addLabel=$ADD_LABEL
 if [[ -n "$LABEL_NAME" ]]; then
-  echo "Warning: Plase define the ADD_LABEL variable instead of the deprecated LABEL_NAME."
+  echo "Warning: Please define the ADD_LABEL variable instead of the deprecated LABEL_NAME."
   addLabel=$LABEL_NAME
 fi
 
@@ -41,6 +41,7 @@ label_when_approved() {
   reviews=$(echo "$body" | jq --raw-output '.[] | {state: .state} | @base64')
 
   approvals=0
+  reviews=0
 
   for r in $reviews; do
     review="$(echo "$r" | base64 -d)"
